f53c2b7eefa48a0eba881e55bc8be11a5b973918
CaptainX4 <130850128+CaptainX4@users.noreply.github.com>
2025-04-18 16:45:45 -0400

    Changed intro. Added beer totals.

--- main.py
+++ main.py
@@ -7,16 +7,12 @@ from control_flow import run_analysis_session, ask_restart
 import pandas as pd
 
 def Beer_List():
-    # Initial welcome message and prompt
     print()
     print("\033[1mWelcome to the Beer List Analysis Platform!\033[0m\n"
           "\n"
-          "Note, for tests that ask what variable you want to use, choices are:\n"
-          "\"Territory,\" \"Brewery,\" \"Beer,\" \"Zak,\" \"Jon,\" \"Had,\" \"Style,\" \"ABV,\" and \"SRC.\"\n"
-          "Depending on the analysis, some of these variables will not work.\n"
-          "Please be patient. You are working with a lot of data. It takes a minute to load.\n")
-    input("Press any key and hit enter to continue: ")
-    print()
+          "We're dealing with a lot of data. Please be patient;\n"
+          "it might take a minute to load.\n")
+
 
     # URLs for the datasets
     url_unAm = "https://docs.google.com/spreadsheets/d/1W4F-UwW8jNdw9ZjL0hf_P53m6BWKU9Xszwt0a2TiyFU/export?format=csv&gid=0"
@@ -28,6 +24,11 @@ def Beer_List():
     Am1_data = load_csv_from_url(url_Am1)
     Am2_data = load_csv_from_url(url_Am2)
 
+    # Get beer totals
+    total_total = int(Unamerican["Beer"].iloc[-1])
+    Zak_total = int(Unamerican["Zak"].iloc[-1].replace(",", ""))
+    Jon_total = int(Unamerican["Jon"].iloc[-1].replace(",", ""))
+
     # Clean datasets
     Unamerican = clean_unamerican_data(Unamerican)
     Am1_data = clean_american_data1(Am1_data)
@@ -36,6 +37,14 @@ def Beer_List():
     # Combine datasets
     American = pd.concat([Am1_data, Am2_data], axis=0)
     All = pd.concat([Unamerican, Am1_data, Am2_data], axis=0)
+
+    # print()
+    # print("\033[1mWelcome to the Beer List Analysis Platform!\033[0m\n"
+    #       "\n"
+    print(f"There are currently \033[1m{total_total:,}\033[0m beers on the list.\n"
+          f"Jon has had \033[1m{Jon_total:,}\033[0m, and Zak has had \033[1m{Zak_total:,}\033[0m.\n")
+    input("Press any key and hit enter to continue: ")
+    print()
 
     data_options = {"Unamerican": Unamerican, "American": American, "All": All}
     data_choice = input("What Beer List data would you like to analyze?\n"
