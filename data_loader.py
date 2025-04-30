95cdf3e86d7c080617f5dba2b40e8a79c8157756
CaptainX4 <130850128+CaptainX4@users.noreply.github.com>
2025-04-30 17:03:57 -0400

    Added general nan date value

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,6 @@
 # data_loader.py
 import certifi, ssl, urllib.request as urllib2
+import numpy as np
 import pandas as pd
 from io import StringIO
 
@@ -10,24 +11,28 @@ def load_csv_from_url(url):
     return pd.read_csv(StringIO(data_str))
 
 def clean_unamerican_data(df):
+    df["Added"] = df["Added"].replace(np.nan, "11/3/2013")
     cleanup_columns = ["Comments", "Country Total", "Zak Total", "Jon Total", "Maps Link"]
     df = df.drop(cleanup_columns, axis=1)
-    df = df[~df["Country"].isin(["US", "Total: this sheet", "Total: all sheets"])]
+    df = df.dropna(subset=["Brewery"], how='all')
     df = df.dropna(how="all")
     return df.rename(columns={"Country": "Territory"})
 
 def clean_american_data1(df):
+    df["Added"] = df["Added"].replace(np.nan, "11/3/2013")
     cleanup_columns = ["Comments", "Zipcode"]
     df = df.drop(cleanup_columns, axis=1)
+    df = df.drop(df.index[-1])
+    df = df.drop(df.index[-1])
+    df = df.drop(df.index[-1])
     df = df.drop(df.index[-1])
     df = df.dropna(how = "all")
     return df.rename(columns = {"State": "Territory"})
 
 def clean_american_data2(df):
+    df["Added"] = df["Added"].replace(np.nan, "11/3/2013")
     cleanup_columns = ["Comments", "Zipcode"]
     df = df.drop(cleanup_columns, axis=1)
-    df = df.drop(df.index[0])
-    df = df.drop(df.index[-1])
-    df = df.drop(df.index[-1])
+    df = df.dropna(subset=["Been To"], how='all')
     df = df.dropna(how="all")
     return df.rename(columns={"State": "Territory"})
